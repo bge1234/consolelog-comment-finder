9802287f074abd6a33000d138960ae0dbb341adc
bge1234 <bge1234@gmail.com>
2017-01-24 17:43:36 -0700

    add samplejavascript.js content

---
+++ samplejavascript.js
@@ -0,0 +1,17 @@
+function sampleJs (arg) {
+  console.log('here is a console log')
+
+  var inBetweenCode = 0
+
+  // This is a comment
+
+  inBetweenCode++
+
+  console.log('arg: ', arg)
+
+  // Another comment
+
+  return inBetweenCode
+}
+
+sampleJs('hi')
